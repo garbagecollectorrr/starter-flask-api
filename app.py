f9c3cdfbee9f6566ef8c9259a369c2ab82871db5
Arun Jayaprakash <garbagecollectorrrrr@gmail.com>
2024-03-21 13:00:38 +0000

    t1
    t1

--- app.py
+++ app.py
@@ -5,4 +5,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+        s =  """<!DOCTYPE html>
+            <html>
+            <head>
+                <title>MoonPay SDK</title>
+                <!--Adds the MoonPay SDK as a script to your HTML file-->
+                <script defer src="https://static.moonpay.com/web-sdk/v1/moonpay-web-sdk.min.js"></script> 
+            </head>
+            <body>
+                <!--Initialize the SDK in your application with the flow, environment, variant, and any parameters related to Buy, Sell or Swap.-->
+                <script>
+                    window.onload = function() {
+                        const moonpaySdk = window.MoonPayWebSdk.init({  
+                            flow: 'buy',
+                            environment: 'sandbox',
+                            variant: 'overlay',  
+                            params: {  
+                                apiKey: 'pk_test_VfrqdFXjXekCEtIc8zijIf50oVJrCaC',
+                                theme: 'dark',
+                                // baseCurrencyCode: 'eur',
+                                // baseCurrencyAmount: '100',
+                                // defaultCurrencyCode: 'eth',
+                                walletAddress: '0x5AdF7A7A70f7CdD86A9e133925A0b323840EE0B4',
+                                externalCustomerId: 'arun'
+                            },
+                            debug: true
+                        });
+
+                        // Show the MoonPay SDK as soon as the page loads
+                        moonpaySdk.show();
+                    }
+                </script>
+            </body>
+            </html>
+        """
+
+    return s
